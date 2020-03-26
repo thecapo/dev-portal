b0e1534811fe0d3dc1d59f4b04fce658ff7a3c30
thecapo <the7thcapo@gmail.com>
2020-03-26 17:17:39 +1100

    npm run build on client

---
+++ client/build/precache-manifest.cccd640616cf69147e988a559589a877.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50dc02da4ab78eff47dabac219eb8d60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6eab2c212f3f995897c",
+    "url": "/static/css/main.ada8c1b2.chunk.css"
+  },
+  {
+    "revision": "a64a24c74f5c989af37e",
+    "url": "/static/js/2.0edb89ac.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.0edb89ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6eab2c212f3f995897c",
+    "url": "/static/js/main.6c1bc1c1.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "b52e3a33c4df85057ba82115d70bae47",
+    "url": "/static/media/hero-image.b52e3a33.jpg"
+  }
+]);
